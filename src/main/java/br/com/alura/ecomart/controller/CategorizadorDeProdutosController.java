7c8308f7ba7585053b56816050af56c54e4e615f
Álvaro Grajau Rodrigues <alvaro.rodrigues@dbserver.com.br>
2025-08-23 11:48:44 -0300

    Modelos e custos

--- src/main/java/br/com/alura/ecomart/controller/CategorizadorDeProdutosController.java
+++ src/main/java/br/com/alura/ecomart/controller/CategorizadorDeProdutosController.java
@@ -1,5 +1,7 @@
 package br.com.alura.ecomart.controller;
 
+import com.knuddels.jtokkit.Encodings;
+import com.knuddels.jtokkit.api.ModelType;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.prompt.ChatOptionsBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +15,12 @@ public class CategorizadorDeProdutosController {
     private final ChatClient chatClient;
 
     public CategorizadorDeProdutosController(ChatClient.Builder chatClientBuilder) {
-        this.chatClient = chatClientBuilder.build();
+        this.chatClient = chatClientBuilder
+                .defaultOptions(ChatOptionsBuilder
+                        .builder()
+                        .withModel("gpt-4o")
+                        .build())
+                .build();
     }
 
     @GetMapping
@@ -34,6 +41,9 @@ public class CategorizadorDeProdutosController {
                 Resposta: Esportes
                 """;
 
+        var tokens = contarTokens(system, produto);
+        System.out.println("Total de Tokens: " +tokens);
+
         return this.chatClient.prompt()
                 .system(system)
                 .user(produto)
@@ -43,6 +53,12 @@ public class CategorizadorDeProdutosController {
                         .build())
                 .call()
                 .content();
+    }
+
+    public int contarTokens(String system, String user) {
+        var registry = Encodings.newDefaultEncodingRegistry();
+        var enc = registry.getEncodingForModel(ModelType.GPT_4O_MINI);
+        return enc.countTokens(system + user);
     }
 
 }
